80119833341cd566429d73accc176ea936fd4e86
Manoj Baruah <manoj.adams@gmail.com>
2025-07-03 00:33:01 +0530

    adding new example: form with mulitple address

---
+++ src/examples/FormWithMultipleAddress.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import MuiForms from "mui-forms";
+import schema from "./schema-multiple-address.json";
+import { useClient } from "@/common/hooks";
+import { Box, createTheme, ThemeProvider } from "@mui/material";
+import styled from "@emotion/styled";
+import { axios } from "@/common/Util";
+import { useRouter } from "next/router";
+import { FORM_THEME } from "@/components/ComponentWithCustomTheme/ComponentWithCustomTheme";
+
+const landingPageTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#fff"
+        }
+    },
+    typography: {
+        fontSize: 12
+    }
+});
+
+const FormWithMultipleAddress = () => {
+    const shouldRender = useClient();
+    const router = useRouter();
+
+    return shouldRender && (
+        <ThemeProvider theme={FORM_THEME}>
+            <Box sx={{backgroundColor: "#fff", margin: "auto", marginBottom: "60px", "borderRadius": "20px"}}>
+                <FormBox>
+                    <MuiForms
+                        config={{
+                            loader: {
+                                enabled: true
+                            }
+                        }}
+                        schema={schema}
+                        onNext={async (data) => {
+                            const response = await axios.post("/submit", data);
+                            return true;
+                        }}
+                        onSubmit={async (data) => {
+                            const response = await axios.post("/submit", data);
+                            router.push("/examples/form-with-basic-details")
+                        }} 
+                    />
+                </FormBox>
+            </Box>
+        </ThemeProvider>
+    )
+}
+
+export default FormWithMultipleAddress;
+
+const FormBox = styled.div`
+    .footer {
+        padding-bottom: 24px;
+    }
+`;
