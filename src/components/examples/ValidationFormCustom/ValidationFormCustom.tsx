a86218f2ee8761f4471adb8884e4d17dfa3cf93b
Manoj Baruah <manoj.adams@gmail.com>
2026-05-07 01:13:01 +0530

    custom validation field

--- src/components/examples/ValidationFormCustom/ValidationFormCustom.tsx
+++ src/components/examples/ValidationFormCustom/ValidationFormCustom.tsx
@@ -6,14 +6,9 @@ import schema from './schema.json';
 
 // Custom validation: from date should not be in future
 const validateFromDate: TValidator = (_value, _validatorArgs) => {
-    const validatorArgs = (_validatorArgs ?? {}) as { maxDate?: string };
-    if (validatorArgs.maxDate !== 'today') {
-        return true;
-    }
-
     const fromDate = new Date(_value as string);
     const currentDate = new Date();
-    if (fromDate.getTime() > currentDate.getTime()) {
+    if (fromDate.getTime() >= currentDate.getTime()) {
         return false;
     }
     return true;
@@ -21,14 +16,11 @@ const validateFromDate: TValidator = (_value, _validatorArgs) => {
 
 // Custom validation: to date should not be before the field passed in validation args
 const validateToDate: TValidator = (_value, _validatorArgs) => {
-    const validatorArgs = (_validatorArgs ?? {}) as { compareWithFieldName?: string };
-    const fromDate = validatorArgs.compareWithFieldName
-        ? metaAPI.metaForm.getFieldValue('custom_validation_form', 'default', validatorArgs.compareWithFieldName)
-        : undefined;
+    const fromDate = metaAPI.metaForm.getFieldValue('custom_validation_form', 'default', 'fromDate');
     const toDate = new Date(_value as string);
     if (fromDate) {
         const fromDateObj = new Date(fromDate as string);
-        if (toDate.getTime() < fromDateObj.getTime()) {
+        if (toDate.getTime() <= fromDateObj.getTime()) {
             return false;
         }
     }
